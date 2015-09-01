626b12c2d378613ba51e7bd15be32e7481d7f9be
Jinxi Wu <jinxi@umich.edu>
2015-08-31 18:15:09 -0700

    First Commit
    Upload the whole file folder

---
+++ data/products.js
@@ -0,0 +1,50 @@
+var foodCollection = [
+  {
+    id: 1,
+    name: 'Burger and Fries',
+    price: 9.99,
+    currency: '$',
+    image: 'images/01.jpg',
+    url: 'http://www.fluc.com/'
+  },
+  {
+    id: 2,
+    name: 'Chicken',
+    price: 10.99,
+    currency: '$',
+    image: 'images/02.jpg',
+    url: 'https://www.fluc.com/'
+  },
+  {
+    id: 3,
+    name: 'Pasta',
+    price: 8.99,
+    currency: '$',
+    image: 'images/03.jpg',
+    url: 'https://www.fluc.com/'
+  },
+  {
+    id: 4,
+    name: 'Salad',
+    price: 7.99,
+    currency: '$',
+    image: 'images/04.jpg',
+    url: 'https://www.fluc.com/'
+  },
+  {
+    id: 5,
+    name: 'Sushi',
+    price: 19.99,
+    currency: '$',
+    image: 'images/05.jpg',
+    url: 'https://www.fluc.com/'
+  },
+  {
+    id: 6,
+    name: 'Ramen',
+    price: 9.99,
+    currency: '$',
+    image: 'images/06.jpg',
+    url: 'https://www.fluc.com/'
+  }
+]
